dcbdaa6a11796b6c1e62dfcc2bcbdfef3c1a778c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2016-04-17 21:57:24 +0900

    correct search initial point algorithm
     our previous algorithm does not work correctly, we found a counter example like below
    00000
    11110
    00010
    00000
    00000

--- terasaki/pixel_tracker.py
+++ terasaki/pixel_tracker.py
@@ -44,11 +44,31 @@ def collect_curve_coordinates(domain):
             coords.append(ind)
     return coords
 
+def surro(dom,c,direc):
+    return not np.all(direc==0) and domain[tuple(c+direc)]==1
+
 def exist_start(domain,coords):
     for c in coords:
-        sub_dom=[domain[tuple(c+np.array([i,j]))] for i in [-1,0,1] for j in [-1,0,1]]
+        sub_dom=np.array([domain[tuple(c+np.array([i,j]))] for i in [-1,0,1] for j in [-1,0,1]]).reshape(3,3)
         if np.count_nonzero(sub_dom)==2:
             return True,c
+        elif np.count_nonzero(sub_dom)==3:
+            y_num=[i for i in range(sub_dom.shape[0]) if np.any(sub_dom[i])==1]
+            x_num=[j for j in range(sub_dom.shape[1]) if np.any(sub_dom[:,j])==1]
+            if len(x_num)==3 or len(y_num)==3:
+                continue
+            ext_sub_dom=[domain[tuple(c+np.array([i,j]))] for i in [-2,-1,0,1,2] for j in [-2,-1,0,1,2]]
+            if np.count_nonzero(ext_sub_dom)==4:
+                candidates=[np.array([i,j]) for i in [-1,0,1] for j in [-1,0,1] if surro(domain,c,np.array([i,j]))]
+                print(candidates)
+                if all([domain[tuple(c+2*candi)]==0 for candi in candidates])==True:
+                    return True,c
+                else :
+                    continue
+            else :
+                continue
+        else:
+            continue
     return False,None
 
 def condi(domain,traced,c,direc):
